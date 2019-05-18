58687b865311efa48793d8fa93e40d5b13b3222b
Stefandy <oscargolfmax@gmail.com>
2019-05-18 20:59:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/notification/AlarmReceiver.java
@@ -0,0 +1,36 @@
+package com.example.notification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationBuilderWithBuilderAccessor;
+
+public class AlarmReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        int notificationId = intent.getIntExtra("notificationId",0);
+        String message = intent.getStringExtra("todo");
+
+        Intent mainIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0 ,mainIntent, 0);
+
+        NotificationManager myNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("Heyy!!!!")
+                .setContentText(message)
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setContentIntent(contentIntent);
+
+
+        myNotificationManager.notify(notificationId, builder.build());
+
+    }
+}
